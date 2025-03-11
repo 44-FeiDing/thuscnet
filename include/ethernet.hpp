96d952148f91e09690d532c72b48c23c2288ebef
Letian Ding <44feiding@gmail.com>
2025-03-11 14:09:25 +0800

    start developing with P10345

---
+++ include/ethernet.hpp
@@ -0,0 +1,24 @@
+#ifndef ETHERNET_HPP_
+#define ETHERNET_HPP_
+#include <array>
+#include <cstdint>
+#include <istream>
+#include <ostream>
+#include <vector>
+using std::istream;
+using std::ostream;
+namespace ETHERNET {
+    class Ethernet
+    {
+        private:
+            std::array<uint8_t, 6> dest;
+            std::array<uint8_t, 6> src;
+            uint16_t ether_type;
+            std::vector<uint8_t> data;
+            std::array<uint8_t, 4> fcs;
+        public:
+            Ethernet(const std::vector<uint8_t> &);
+            bool verify();
+    }__attribute__((packed));
+}
+#endif
--- src/ethernet.cpp
+++ src/ethernet.cpp
@@ -1,4 +1,17 @@
-class ethernet
-{
+#include "ethernet.hpp"
+#include <cstdint>
+#include <iostream>
+#include <vector>
 
+namespace ETHERNET
+{
+    Ethernet::Ethernet(const std::vector<uint8_t> & src)
+    {
+        if (src.size() < 64)
+        {
+            std::cerr << "Source data too small when construct ethernet ";
+            return;
+        }
+        //to be continue...
+    }
 }
